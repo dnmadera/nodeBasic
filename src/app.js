77e3dc1a397bf5f76253e61e79418dad4adc52d5
dnmadera <dnmadera@gmail.com>
2022-04-17 17:40:02 -0300

    agrega ignore

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-// Se carga el módulo de HTTP
-var http = require("http");
+var express = require('express');
+var app = express();
 
-// Creación del servidor HTTP, y se define la escucha
-// de peticiones en el puerto 8000
-http.createServer(function(request, response) {
+app.get('/', function(req, res) {
+  res.send('Hola Mundo!');
+});
 
-   // Se define la cabecera HTTP, con el estado HTTP (OK: 200) y el tipo de contenido
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/user/4', function(req, res) {
+    res.send('User 4!');
+  });
 
-   // Se responde, en el cuerpo de la respuesta con el mensaje "Hello World"
-   response.end('Hola Mundo!\n');
-}).listen(8000);
-
-// Se escribe la URL para el acceso al servidor
-console.log('Servidor en la url http://127.0.0.1:8000/');
+app.listen(3000, function() {
+  console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
